81e1b305a1d9ffb00ae9e330a36e14dafce83884
SoniRe <helloadityayo@gmail.com>
2024-01-08 00:12:35 +0530

    Swiggy API issue 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 const HOME_PAGE_URL =
-  "/api1?lat=28.6028557&lng=77.08198850000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "/restaurants?lat=28.6028557&lng=77.08198850000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export { CDN_URL, HOME_PAGE_URL };
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -9,7 +9,7 @@ const useRestaurantMenu = (resId) => {
 
   const fetchData = async () => {
     const data = await fetch(
-      `/api2?page-type=REGULAR_MENU&complete-menu=true&lat=28.5987633&lng=77.0786143&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
+      `/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.5987633&lng=77.0786143&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
     );
 
     const json = await data.json();
